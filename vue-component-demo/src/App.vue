66e985d43c6acab9b8b2ce3989db871226003a17
Opeyemi <Opeyemi5055@yahoo.com>
2025-04-28 16:57:58 +0100

    moved to project

---
+++ vue-component-demo/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Hello World!</div>
+  <div>{{ message }}</div>
+</template>
+
+<script>
+  export default{
+    setup(){
+      const message = "Hello Vue!js 3!"
+      return { message };
+    }
+  }
+</script>
+
+<style></style>
