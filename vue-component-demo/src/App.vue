59c45d4aad13f92a0a25b77b7ba4c429e10657ea
Opeyemi <Opeyemi5055@yahoo.com>
2025-05-07 23:05:49 +0100

    component contd

--- vue-component-demo/src/App.vue
+++ vue-component-demo/src/App.vue
@@ -1,15 +1,50 @@
 <template>
-  <div>Hello World!</div>
-  <div>{{ message }}</div>
+  <div class="container">
+    <div>{{ message }}</div>
+    <div class="row">
+      <div class="col-12" v-for="contact in contacts" :key="contact.name">
+        <Contact
+          :name="contact.name"
+          :phone="contact.phone"
+          :ownername="contact.ownerName"
+          :email="contact.email"
+          :isFavorite="contact.isFavorite"
+          @update-favorite="contact.isFavorite = !contact.isFavorite"
+        ></Contact>
+      </div>
+    </div>
+  </div>
+
 </template>
 
-<script>
-  export default{
-    setup(){
-      const message = "Hello Vue!js 3!"
-      return { message };
+<script setup>
+  import {ref, reactive } from 'vue';
+  import Contact from './components/Contact.vue';
+  const message = "Hello Vue"
+  const ownerName = ref("Jembe")
+  const contacts = reactive([
+    {
+      name: "Opeyemi",
+      phone:123456789,
+      ownerName: ownerName,
+      email:"Opeyemi@example.com",
+      isFavorite: true
+    },
+    {
+      name:"Ben",
+      phone:5551234567,
+      ownerName: ownerName,
+      email:"ben@example.com",
+      isFavorite: false
+    },
+    {
+      name:"Mike",
+      phone:6661234567,
+      ownerName: ownerName,
+      email:"mike@example.com",
+      isFavorite: false
     }
-  }
+  ]);
 </script>
 
 <style></style>
---
+++ vue-component-demo/src/components/Contact.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="bg-info rounded p-1 pb-4 m-1">
+    <div class="row">
+      <div class="col-6">
+        <h3>Name : {{ name }}</h3>
+        <p>Email : {{ email }}</p>
+        <p>Phone : {{ phone }}</p>
+      </div>
+      <div @click="toggleFavorite" class="col-3">
+        <button :class="[isFavorite ? 'btn btn-warning' : 'btn btn-success']">
+          {{ isFavorite ? "Remove from" : "Add to" }} Favorite
+        </button>
+      </div>
+    </div>
+    <span class="float-end small" v-if="ownername != ''">
+      this contact is owned by {{ name }}
+    </span>
+  </div>
+</template>
+
+<script setup>
+ // defineProps and ref might not be imported but will still work 
+  const props = defineProps({
+    name: {type: String, required: true},
+    phone: Number, 
+    ownername: String,
+    email: {type: String, required: false, default:"hello@examplevue.com"},
+    isFavorite: Boolean,
+  });
+
+  const emit = defineEmits(["update-favorite"]);
+
+  function toggleFavorite() {
+    emit("update-favorite")
+  };
+</script>
