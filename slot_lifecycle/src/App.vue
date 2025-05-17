ca3f00fe51dc4d27ce3585bbe7ed79b019a13b01
Opeyemi <Opeyemi5055@yahoo.com>
2025-05-17 15:52:30 +0100

    slotLigecycle

---
+++ slot_lifecycle/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="bg text pt-3" :style="{height: '100vh'}">
+    <h1 class="text-center text-success">ContactOpedia</h1>
+    <div class="container">
+      <LuckyNumber></LuckyNumber>
+      <br/>
+      <LuckyNumberV2></LuckyNumberV2>
+    </div>
+  </div>
+</template>
+
+<script setup>
+  import {ref, reactive } from 'vue';
+  import LuckyNumber from "./components/LuckyNumber.vue";
+  import LuckyNumberV2 from "./components/LuckyNumberV2.vue";
+</script>
+
+<style></style>
